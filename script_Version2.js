84b4c5f3d0c38b3a4b9843a21eafec91884a9deb
ryesooo <1705-raihanah@ipgkdri.edu.my>
2025-05-12 04:25:07 +0800

    Add files via upload

---
+++ script_Version2.js
@@ -0,0 +1,13 @@
+const phrases = [
+  "Would you mind taking a quick picture for me?",
+  "Would you mind helping me out with a photo?",
+  "Could you snap a picture of me, please?",
+  "I don't feel good. My ..... hurts.",
+  "I am not feeling well. My ...... hurts.",
+  "How much does this cost?",
+  "Could you tell me how much this is?",
+  "Could you give me a better price for this?",
+  "Can I get a small discount?",
+  "I would like to order .......",
+  "Can I have...?"
+];
